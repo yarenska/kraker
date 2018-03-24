aaac631bdd2b769f2388db7393880dada793638c
yarenska <32244062+yarenska@users.noreply.github.com>
2018-03-24 18:27:27 +0200

    Update challenge_15.py

--- cryptopals/challenge_15.py
+++ cryptopals/challenge_15.py
@@ -35,6 +35,6 @@ if __name__ == '__main__':
     text = "YELLOW SUBMARINE"
     paddedtext = PKCS_7(text,20) 
     unpadded = isValidPKCS_7(paddedtext) #or "ICE ICE BABY\x05\x05\x05\x05" or "ICE ICE BABY\x01\x02\x03\x04".
-                                         #they are not correct thus even before coming to print part of the main function,
+                                         #they are not correct, so even before coming to print part of the main function,
                                          #it will throw an exception at isValidPKCS_7
     print(unpadded)
